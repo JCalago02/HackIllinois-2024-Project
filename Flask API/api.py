38b913595b3cd83b65517fe640f55b903ec14857
Jericho Calago <jcalago02@gmail.com>
2024-02-24 19:47:51 -0600

    Add flask api

---
+++ Flask API/api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home"
+
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id" : user_id,
+        "name" : "John Doe",
+        "email" : "email@example.come"
+    }
+if __name__ == "__main__":
+    app.run(debug=True)
