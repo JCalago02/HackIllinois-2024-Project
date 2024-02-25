6852e7d253b0cbba68c6693348bf280aaeab81e3
Jericho Calago <jcalago02@gmail.com>
2024-02-25 03:13:20 -0600

    Add testfile.py

---
+++ testfile.py
@@ -0,0 +1,13 @@
+def twoSum(nums, target):
+    numMap = {}
+    n = len(nums)
+
+    for i in range(n):
+        complement = target - nums[i]
+        if complement in numMap:
+            return [numMap[complement], i]
+        numMap[nums[i]] = i
+
+    return []  # No solution found
+
+print(twoSum([1, 2, 3, 4, 5], 5))
