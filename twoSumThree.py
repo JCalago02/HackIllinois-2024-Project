7a806c912cdf9c34db40a48eb3e6aac8fdea67d0
Jericho Calago <jcalago02@gmail.com>
2024-02-25 08:59:26 -0600

    Add twosum3

---
+++ twoSumThree.py
@@ -0,0 +1,11 @@
+def two_sum(nums, target):
+    num_map = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], i]
+        num_map[num] = i
+    return None
+
+
+print(two_sum([1,2,3,4,5], 4))
